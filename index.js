0976ac1207e8769ce3722c44bb81deb9c8f48bb9
Virak Both <virak.sieng.007@gmail.com>
2024-07-25 13:58:01 +0700

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, World!');
+});
+
+app.get('/user/:id', (req, res) => {
+  res.status(200).json({ id: req.params.id });
+});
+
+module.exports = app;
